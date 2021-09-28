6e518d56f38655eb2cd570e987fef3a6999acdf7
Derek Bracken <derekbracken@gmail.com>
2021-09-28 15:49:58 +0100

    Added Borrower class and test file

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
+
+    private Borrower borrower;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+
+
+    @Before
+    public void Borrower(){
+        borrower = new Borrower();
+        book1 = new Book("H2G2", "Duglas Adams", "Sci-Fi");
+        book2 = new Book("Casino Royale", "Ian Fleming", "Thriller");
+        book3 = new Book("Angles and Demons", "Dan Brown", "Thriller");
+    }
+
+    @Test
+    public void hasCollection(){
+        assertEquals(0, borrower.getCollection());
+    }
 }
