779687af26add19de4f53bdf70b2df238be2fdb8
Derek Bracken <derekbracken@gmail.com>
2021-09-28 15:20:05 +0100

    Library class and test set up

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> stock;
+    private Integer capacity;
+
+    public Library(){
+        this.stock = new ArrayList<>();
+        this.capacity = 10;
+    }
+
+    public int getStock() {
+        return this.stock.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book1 = new Book("H2G2", "Duglas Adams", "Sci-Fi");
+        book2 = new Book("Casino Royale", "Ian Fleming", "Thriller");
+        book3 = new Book("Angles and Demons", "Dan Brown", "Thriller");
+    }
+
+    @Test
+    public void canGetStock(){
+        assertEquals(0, library.getStock());
+    }
 }
